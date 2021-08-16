ea68cbbb82be63c7b69272e5bd736e70e3128236
kunal-rtf <kunal@raisingthefloor.org>
2021-08-16 17:43:47 +0530

    Database initial commit

--- Morphic.Focus/Core/Session.cs
+++ Morphic.Focus/Core/Session.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,78 @@ namespace Morphic.Focus.Core
 {
     class Session
     {
+        //Backbone. Will be modified for data-binding
+        private int _id;
+
+        private DateTime _actualStartTime;
+
+        private DateTime _actualEndTime;
+
+        private string _userName;
+
+        private int _scheduleID;
+
+        private DateTime _lastStartTime;
+
+        private DateTime _lastBreakStartTime;
+
+        private string _focusType;
+
+        public string FocusType
+        {
+            get { return _focusType; }
+            set { _focusType = value; }
+        }
+
+
+        public DateTime LastBreakStartTime
+        {
+            get { return _lastBreakStartTime; }
+            set { _lastBreakStartTime = value; }
+        }
+
+
+        public DateTime LastStartTime
+        {
+            get { return _lastStartTime; }
+            set { _lastStartTime = value; }
+        }
+            
+
+        public int ScheduleID
+        {
+            get { return _scheduleID; }
+            set { _scheduleID = value; }
+        }
+
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+
+        public DateTime ActualEndTime
+        {
+            get { return _actualEndTime; }
+            set { _actualEndTime = value; }
+        }
+
+
+        public DateTime ActualStartTime
+        {
+            get { return _actualStartTime; }
+            set { _actualStartTime = value; }
+        }
+
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
     }
 }
--- Morphic.Focus/Core/SessionDetails.cs
+++ Morphic.Focus/Core/SessionDetails.cs
@@ -8,5 +8,75 @@ namespace Morphic.Focus.Core
 {
     class SessionDetails
     {
+        //Backbone. Will be modified for data-binding
+        private int _id;
+
+        private int _sessionID;
+
+        private bool _isDnDON;
+
+        private bool _isBreakON;
+
+        private int _breakMins;
+
+        private int _breakFreqMins { get; set; }
+
+        private bool _isBlockListOn;
+
+        private int _blockListID;
+
+        public int BlockListID
+        {
+            get { return _blockListID; }
+            set { _blockListID = value; }
+        }
+
+
+        public bool IsBlockListOn
+        {
+            get { return _isBlockListOn; }
+            set { _isBlockListOn = value; }
+        }
+
+
+        public int BreakMins
+        {
+            get { return _breakMins; }
+            set { _breakMins = value; }
+        }
+
+        public int BreakFreqMins
+        {
+            get { return _breakFreqMins; }
+            set { _breakFreqMins = value; }
+        }
+
+
+        public bool IsBreakON
+        {
+            get { return _isBreakON; }
+            set { _isBreakON = value; }
+        }
+
+
+        public bool IsDnDON
+        {
+            get { return _isDnDON; }
+            set { _isDnDON = value; }
+        }
+
+        public int SessionID
+        {
+            get { return _sessionID; }
+            set { _sessionID = value; }
+        }
+
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
     }
 }
